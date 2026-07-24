54213dded8eff2ad648394e2277a17549c3ddb7e
Joabe Oliveira <joabeantonio@gmail.com>
2026-07-24 10:42:37 -0300

    Adiciona interface web para teste dos endpoints

--- api/src/index-pg.js
+++ api/src/index-pg.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas da API
 app.use('/api/pesquisa', pesquisaRoutes);
